d82e1ae017880322a841f7530837a21dd1b103bc
Ashutoshh <ashutoshh@linux.com>
2026-07-21 16:56:48 +0530

    updates day2 AIops

--- day2/list_models.py
+++ day2/list_models.py
@@ -8,5 +8,12 @@ for i in dir(client):
     if "list" in i:
         print(i)
 # printing listing foundation model 
-models=client.list_foundation_models()
-print(models)
+models=client.list_foundation_models(
+    byProvider="Anthropic"
+)
+print(models["modelSummaries"])
+# complete this -- print all antropic related -- Modelid , name , input , output Modalities
+
+for i in models["modelSummaries"]:
+    print(f" Model ID   :  {i['modelId']}")
+    print(f" Model Name   :  {i['modelName']}")
